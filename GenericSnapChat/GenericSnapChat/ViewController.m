5a7577cb6d5869d3f27d82d535d23a50621b4343
Daniel Williamson <d.williamson@stroma.com>
2014-07-14 12:25:18 +0100

    Working on Login Screen

--- GenericSnapChat/GenericSnapChat/ViewController.m
+++ GenericSnapChat/GenericSnapChat/ViewController.m
@@ -17,6 +17,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    //Create an animation with pulsating effect
+    CABasicAnimation *theAnimation;
+    
+    //within the animation we will adjust the "opacity"
+    //value of the layer
+    theAnimation=[CABasicAnimation animationWithKeyPath:@"opacity"];
+    //animation lasts 0.4 seconds
+    theAnimation.duration=1.5;
+    //and it repeats forever
+    //we want a reverse animation
+    //justify the opacity as you like (1=fully visible, 0=unvisible)
+    theAnimation.fromValue=[NSNumber numberWithFloat:0.0];
+    theAnimation.toValue=[NSNumber numberWithFloat:1.0];
+
+    
+    //Assign the animation to your UIImage layer and the
+    //animation will start immediately
+    [self.fb_icon.layer addAnimation:theAnimation
+                           forKey:@"animateOpacity"];
+    self.fb_icon.layer.shouldRasterize = YES;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +46,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
